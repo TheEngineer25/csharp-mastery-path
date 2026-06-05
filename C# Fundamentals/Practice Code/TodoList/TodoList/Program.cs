57a877febda58bac5e43192a2f6187570fbe59d2
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-06-05 15:27:09 +0300

    vault backup: 2026-06-05 15:27:09

--- C# Fundamentals/Practice Code/TodoList/TodoList/Program.cs
+++ C# Fundamentals/Practice Code/TodoList/TodoList/Program.cs
@@ -1,3 +1,10 @@
 ﻿Console.WriteLine("Hello!");
+Console.WriteLine("What you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
 
 Console.ReadKey();
