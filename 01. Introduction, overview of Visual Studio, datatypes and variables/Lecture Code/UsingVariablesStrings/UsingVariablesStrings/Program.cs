02c330f254b1f8ac2131a0bb2b503e580242ce02
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-06-08 15:51:22 +0300

    vault backup: 2026-06-08 15:51:22

---
+++ 01. Introduction, overview of Visual Studio, datatypes and variables/Lecture Code/UsingVariablesStrings/UsingVariablesStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+// declare a variable
+string myFriendsname;
+
+//assign a value to the variable
+myFriendsname = "Stefan";
+
+//use the variable
+Console.WriteLine(myFriendsname);
+
+Console.ReadKey();
