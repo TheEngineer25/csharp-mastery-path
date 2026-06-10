2ea414ee748297cefbe377d09cc5d0052809e8b0
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-06-10 11:09:45 +0300

    vault backup: 2026-06-10 11:09:44

--- 01. Introduction, overview of Visual Studio, Datatypes and variables/Code/07. Variables-strings/07. Variables-strings/Program.cs
+++ 01. Introduction, overview of Visual Studio, Datatypes and variables/Code/07. Variables-strings/07. Variables-strings/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// declare a variable
+string myFriendsName;
+
+//assign a value to the variablre
+myFriendsName = "Stefan";
+
+// use the variable
+Console.WriteLine(myFriendsName);
+
+//overwrite a variable
+myFriendsName = "Viktor";
+Console.WriteLine(myFriendsName);
+
+string myFriendsName2 = "Lachezar";
