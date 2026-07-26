bad05c7414ae465d1665ea4a5d688f67414d5a6d
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-07-26 17:22:56 +0300

    vault backup: 2026-07-26 17:22:56

---
+++ Лекции/1. Въведение, преглед на Visual Studio, типове данни и променливи/14. Преглед на типове данни/Код/DatatypesOverview/DatatypesOverview/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+
+// Reference type
+string myName = "Engineer25";
+
+// Value types
+int myAge = -35;
+float pi = 3.14f;
+
+byte age = 25;
+
+short linkedinConnections = 32500;
+
+long phoneNumber = 01234567890;
